f65077c0817c73fcbe849bf7b5e24682a43c88b9
angelinejerald <jangelinemahiba@gmail.com>
2021-03-13 21:28:53 +0530

    Update create_kube.sh

--- create_kube.sh
+++ create_kube.sh
@@ -4,5 +4,5 @@ then
 	echo "Infrastructure exists"
 else
 	echo "Creating cluster"
-	eksctl create cluster --name capstone-kube --version 1.19 --region us-west-2 --nodegroup-name capstone-kube --node-type t2.micro --nodes-min 2 --nodes-max 4 --managed
+	eksctl create cluster --name capstone-kube --version 1.19 --region us-west-2 --nodegroup-name capstone-kube --node-type t3.medium --nodes-min 2 --nodes-max 4 --managed
 fi
